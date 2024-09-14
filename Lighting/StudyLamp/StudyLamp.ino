e28114dec179ca60e265e526e8c3d4933d3cd837
Frank2229 <78129526+Frank2229@users.noreply.github.com>
2024-09-14 13:41:42 -0400

    Update StudyLamp.ino

--- Lighting/StudyLamp/StudyLamp.ino
+++ Lighting/StudyLamp/StudyLamp.ino
@@ -13,6 +13,7 @@
 #include <Wire.h>
 #include "time.h"
 
+#define LED 4
 #define RELAY 13
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
@@ -22,8 +23,16 @@ const int gmtOffsetMult = -5;
 const int daylightOffsetMult = 1;
 const int studyTimeStart_Hour = 18;
 const int studyTimeStart_Minute = 0;
-const int studyTimeEnd_Hour = 21;
+const int studyTimeEnd_Hour = 20;
 const int studyTimeEnd_Minute = 59;
+
+// INITIALIZATION OF TOUCH SENSOR VARIABLES.
+const int TOUCHSENSOR_MANUAL = 15;
+const int TOUCHSENSOR_TOGGLE = 2;
+bool isManualPressed = false;
+bool isManualMode = false;
+bool isTogglePressed = false;
+bool isToggleOn = false;
 
 // WIFI VARIABLES
 const char* ssid = "Stevens-IoT";
@@ -36,6 +45,7 @@ bool isStudyTime = false;
 int hour = 0;
 int minute = 0;
 char* dayPart;
+int secondIncrementor = 0;
 
 // SETUP OLED DISPLAY
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -43,9 +53,17 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
   Serial.begin(115200);
 
+  // Initialization of the LED.
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
   // Initialization of the lamp relay.
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
+
+  // Initialization of the touch sensors.
+  pinMode(TOUCHSENSOR_MANUAL, INPUT);
+  pinMode(TOUCHSENSOR_TOGGLE, INPUT);
 
   // Initialization of the OLED w/ introduction message.
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
@@ -75,33 +93,70 @@ void setup() {
 }
 
 void loop() {
-  assessStudyTime();
+  if (digitalRead(TOUCHSENSOR_MANUAL) == HIGH) isManualPressed = true;
 
-  if (isStudyTime) digitalWrite(RELAY, HIGH);
-  else digitalWrite(RELAY, LOW);
-
-  // Format to normal am/pm time.
-  if (hour > 12) {
-    hour -= 12;
-    dayPart = "pm";
+  if (isManualPressed && digitalRead(TOUCHSENSOR_MANUAL) == LOW) {
+    if (isManualMode) {
+      isManualMode = false;
+      digitalWrite(LED, LOW);
+    }
+    else {
+      isManualMode = true;
+      digitalWrite(LED, HIGH);
+    }
+    isManualPressed = false;
   }
-  else if (hour == 0) {
-    dayPart = "am";
-    hour = 12;
-  }
-  else if (hour == 12) dayPart = "pm";
-  else dayPart = "am";
-  display.clearDisplay();
-  display.setTextSize(3);
-  display.setCursor(10, 7);
-  display.print(hour);
-  display.print(":");
-  if (minute < 10) display.print("0");
-  display.print(minute);
-  display.print(dayPart);
-  display.display();
 
-  delay(2000);
+  if (isManualMode) {
+    if (digitalRead(TOUCHSENSOR_TOGGLE) == HIGH) isTogglePressed = true;
+
+    Serial.println("MANUAL MODE");
+
+    if (isTogglePressed && digitalRead(TOUCHSENSOR_TOGGLE) == LOW) {
+      if (isToggleOn) {
+        isToggleOn = false;
+        digitalWrite(RELAY, LOW);
+      }
+      else {
+        isToggleOn = true;
+        digitalWrite(RELAY, HIGH);
+      }
+      isTogglePressed = false;
+    }
+  }
+
+  if (secondIncrementor == 200000) {
+    assessStudyTime();
+
+    if (!isManualMode) {
+      if (isStudyTime) digitalWrite(RELAY, HIGH);
+      else digitalWrite(RELAY, LOW);
+    }
+
+    // Format to normal am/pm time.
+    if (hour > 12) {
+      hour -= 12;
+      dayPart = "pm";
+    }
+    else if (hour == 0) {
+      dayPart = "am";
+      hour = 12;
+    }
+    else if (hour == 12) dayPart = "pm";
+    else dayPart = "am";
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setCursor(8, 7);
+    display.print(hour);
+    display.print(":");
+    if (minute < 10) display.print("0");
+    display.print(minute);
+    display.print(dayPart);
+    display.display();
+  
+    secondIncrementor = 0;
+  }
+  else secondIncrementor++;
 }
 
 // This function will initially set the current time retrieved from the internet.
